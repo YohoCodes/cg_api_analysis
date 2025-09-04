0f8115b9ff958be6fe2d912d13fccdcc323979d5
YohoCodes <isaacyoho99@gmail.com>
2025-09-04 02:18:13 +0100

    Updated documentation. Saved More Datasets

--- analysis_ex1.py
+++ analysis_ex1.py
@@ -3,7 +3,7 @@ from chart_analysis import analyze_coin_market_chart
 # This example demonstrates the basic usage of the analyze_coin_market_chart class.
 # We use limit = 5 to analyze the top 5 cryptocurrencies by market cap.
 # This is useful for quick analysis of the most popular coins.
-coin_analysis = analyze_coin_market_chart(limit = 5)
+coin_analysis = analyze_coin_market_chart(limit = 15)
 
 print(coin_analysis.price_correlation())
 print('\n')
